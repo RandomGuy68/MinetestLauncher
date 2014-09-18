fefaffd078b4d0844c6d6863bf41998e590142bb
Nicholas Jones <awesomecoding@gmail.com>
2014-09-18 09:21:10 -0700

    Uploaded base code
    Uploaded the first version code

---
+++ src/com/awesomecoding/minetestlauncher/FileGetter.java
@@ -0,0 +1,166 @@
+package com.awesomecoding.minetestlauncher;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+
+public class FileGetter {
+	
+	String dir;
+	
+	public FileGetter(String directory){
+		this.dir = directory;
+	}
+	
+	public void download(String url, String filedir, String filename, JLabel label, JProgressBar progress, JButton button, String version){
+		try{
+			label.setText("Starting Download...");
+			button.setText("Downloading...");
+			button.setEnabled(false);
+			
+			URL website = new URL(url);
+			
+			URLConnection con = website.openConnection();
+			int total = con.getContentLength();
+			
+			InputStream instream = website.openStream();
+			File file = new File(filedir);
+			file.mkdirs();
+			FileOutputStream outstream = new FileOutputStream(filedir + "\\" + filename);
+			
+			byte[] buffer = new byte[153600];
+	        int totalBytesRead = 0;
+	        int bytesRead = 0;
+
+	        while((bytesRead = instream.read(buffer)) > 0){
+	        	outstream.write(buffer, 0, bytesRead);
+	        	buffer = new byte[153600];
+	        	totalBytesRead += bytesRead;
+	        	double prog = Math.round(((double)totalBytesRead/total)*100);
+	        	label.setText("Downloading... (" + (int)prog + "%)");
+	        	progress.setValue((int)prog);
+	        }
+	        
+	        label.setText("Unzipping...");
+	        button.setText("Unzipping...");
+	        unzip(filedir + "\\" + filename, System.getProperty("user.home") + "\\minetest\\", true);
+	        
+	        PrintWriter writer = new PrintWriter(filedir+"../version.txt", "UTF-8");
+	        writer.println(version);
+	        writer.close();
+	        
+	        label.setText("Launching...");
+	        button.setText("Launching...");
+	        
+	        outstream.close();
+	        instream.close();
+	        
+	        Runtime.getRuntime().exec(System.getProperty("user.home") + "\\minetest\\minetest-" + version + "\\bin\\minetest.exe");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String getContents(String url, boolean seperate){
+		try{
+			String lineSeperator = "";
+			if(seperate)
+				lineSeperator = "\n";
+			URLConnection connection = new URL(url).openConnection();
+			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+			connection.connect();
+
+			BufferedReader r  = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName("UTF-8")));
+
+			String sb = "";
+			String line;
+			while ((line = r.readLine()) != null) {
+			    sb += line + lineSeperator;
+			}
+			return sb;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public String getLocalContents(File file, boolean seperate){
+		try{
+			String lineSeperator = "";
+			if(seperate)
+				lineSeperator = "\n";
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(new BufferedInputStream(new FileInputStream(file))));
+
+		    String line;
+		    String response = "";
+
+		    while ((line = reader.readLine()) != null) {
+		        response = line + lineSeperator;
+		    }
+		    
+		    reader.close();
+		    return response;
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void unzip(String fileloc, String dir, boolean delete){
+		try {
+            // Open the zip file
+            ZipFile zipFile = new ZipFile(fileloc);
+            Enumeration<?> enu = zipFile.entries();
+            while (enu.hasMoreElements()) {
+                ZipEntry zipEntry = (ZipEntry) enu.nextElement();
+
+                String name = zipEntry.getName();
+
+                // Do we need to create a directory ?
+                File file = new File(dir+name);
+                if (name.endsWith("/")) {
+                    file.mkdirs();
+                    continue;
+                }
+
+                File parent = file.getParentFile();
+                if (parent != null) {
+                    parent.mkdirs();
+                }
+
+                // Extract the file
+                InputStream is = zipFile.getInputStream(zipEntry);
+                FileOutputStream fos = new FileOutputStream(file);
+                byte[] bytes = new byte[1024];
+                int length;
+                while ((length = is.read(bytes)) >= 0) {
+                    fos.write(bytes, 0, length);
+                }
+                is.close();
+                fos.close();
+
+            }
+            zipFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+}
---
+++ src/com/awesomecoding/minetestlauncher/Main.java
@@ -0,0 +1,165 @@
+package com.awesomecoding.minetestlauncher;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextPane;
+import javax.swing.SpringLayout;
+import javax.swing.UIManager;
+
+import org.apache.commons.io.FileUtils;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class Main {
+
+	private JFrame frmMinetestLauncherV;
+	private FileGetter fileGetter;
+	
+	private String latest;
+	private String currentVersion;
+	private String changelog;
+	
+	private String userhome = System.getProperty("user.home");
+	
+	private boolean newVersion = false;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main window = new Main();
+					window.frmMinetestLauncherV.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public Main() {
+		initialize();
+	}
+
+	private void initialize() {
+		fileGetter = new FileGetter(userhome + "\\minetest\\temp\\");
+		latest = fileGetter.getContents("http://socialmelder.com/minetest/latest.txt", true);
+		currentVersion = latest.split("\n")[0];
+		changelog = fileGetter.getContents("http://socialmelder.com/minetest/changelog.html", false);
+		
+		try{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		frmMinetestLauncherV = new JFrame();
+		frmMinetestLauncherV.setResizable(false);
+		frmMinetestLauncherV.setIconImage(Toolkit.getDefaultToolkit().getImage(Main.class.getResource("/com/awesomecoding/minetestlauncher/icon.png")));
+		frmMinetestLauncherV.setTitle("Minetest Launcher (Version 0.1)");
+		frmMinetestLauncherV.setBounds(100, 100, 720, 480);
+		frmMinetestLauncherV.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		frmMinetestLauncherV.getContentPane().setLayout(springLayout);
+		
+		final JProgressBar progressBar = new JProgressBar();
+		springLayout.putConstraint(SpringLayout.WEST, progressBar, 10, SpringLayout.WEST, frmMinetestLauncherV.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, progressBar, -10, SpringLayout.SOUTH, frmMinetestLauncherV.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, progressBar, -130, SpringLayout.EAST, frmMinetestLauncherV.getContentPane());
+		frmMinetestLauncherV.getContentPane().add(progressBar);
+		
+		final JButton btnDownloadPlay = new JButton("Play!");
+		springLayout.putConstraint(SpringLayout.WEST, btnDownloadPlay, 6, SpringLayout.EAST, progressBar);
+		springLayout.putConstraint(SpringLayout.SOUTH, btnDownloadPlay, 0, SpringLayout.SOUTH, progressBar);
+		springLayout.putConstraint(SpringLayout.EAST, btnDownloadPlay, -10, SpringLayout.EAST, frmMinetestLauncherV.getContentPane());
+		frmMinetestLauncherV.getContentPane().add(btnDownloadPlay);
+		
+		final JLabel label = new JLabel("Ready to play!");
+		springLayout.putConstraint(SpringLayout.WEST, label, 10, SpringLayout.WEST, frmMinetestLauncherV.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, btnDownloadPlay, 0, SpringLayout.NORTH, label);
+		springLayout.putConstraint(SpringLayout.SOUTH, label, -37, SpringLayout.SOUTH, frmMinetestLauncherV.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, progressBar, 6, SpringLayout.SOUTH, label);
+		frmMinetestLauncherV.getContentPane().add(label);
+		
+		JTextPane txtpnNewFeatures = new JTextPane();
+		txtpnNewFeatures.setBackground(SystemColor.window);
+		springLayout.putConstraint(SpringLayout.NORTH, txtpnNewFeatures, 10, SpringLayout.NORTH, frmMinetestLauncherV.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, txtpnNewFeatures, 10, SpringLayout.WEST, frmMinetestLauncherV.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, txtpnNewFeatures, -10, SpringLayout.NORTH, btnDownloadPlay);
+		springLayout.putConstraint(SpringLayout.EAST, txtpnNewFeatures, 0, SpringLayout.EAST, btnDownloadPlay);
+		txtpnNewFeatures.setEditable(false);
+		txtpnNewFeatures.setContentType("text/html");
+		txtpnNewFeatures.setText(changelog);
+		txtpnNewFeatures.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		frmMinetestLauncherV.getContentPane().add(txtpnNewFeatures);
+		
+		File file = new File(userhome + "\\minetest\\version.txt");
+		
+		if(!file.exists())
+			newVersion = true;
+		else{
+			String version = fileGetter.getLocalContents(file, false);
+			if(!version.equals(currentVersion))
+				newVersion = true;
+		}
+		
+		if(newVersion){
+			label.setText("New Version Available! (" + currentVersion + ")");
+			btnDownloadPlay.setText("Download & Play");
+			
+			btnDownloadPlay.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					Thread t = new Thread(){
+						public void run(){
+							File file = new File(userhome + "\\minetest\\version.txt");
+							String version = fileGetter.getLocalContents(file, false);
+							try{
+								FileUtils.deleteDirectory(new File(userhome + "\\minetest\\minetest-" + version));
+							} catch(Exception e) {
+								e.printStackTrace();
+							}
+							fileGetter.download(latest.split("\n")[1], userhome + "\\minetest\\temp\\", "minetest.zip", label, progressBar, btnDownloadPlay, currentVersion);
+							try {
+								label.setText("Cleaning up...");
+								btnDownloadPlay.setText("Cleaning up...");
+								FileUtils.deleteDirectory(new File(userhome + "\\minetest\\temp"));
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+							System.exit(0);
+						}
+					};
+					t.start();
+				}
+			});
+		} else {
+			btnDownloadPlay.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent arg0) {
+					try {
+						label.setText("Launching...");
+						btnDownloadPlay.setEnabled(false);
+						btnDownloadPlay.setText("Launching...");
+						File file = new File(userhome + "\\minetest\\version.txt");
+						String version = fileGetter.getLocalContents(file, false);
+						Runtime.getRuntime().exec(userhome + "\\minetest\\minetest-" + version + "\\bin\\minetest.exe");
+						System.exit(0);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+			progressBar.setValue(100);
+		}
+	}
+}
